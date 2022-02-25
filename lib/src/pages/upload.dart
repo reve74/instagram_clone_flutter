06ede35adb7aafa8c56ccc5d0cd0fc8744681695
reve74 <reve99@naver.com>
2022-02-25 22:18:43 +0900

    upload ui

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter/src/components/image_data.dart';
 
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(IconsPath.closeImage),
+          ),
+        ),
+        title: Text('New Post'),
+      ),
     );
   }
 }
