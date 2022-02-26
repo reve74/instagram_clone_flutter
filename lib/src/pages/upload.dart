2f2c83f4a1ce3d6f3cc14e14574a63d0ca0b7f8d
reve74 <reve99@naver.com>
2022-02-26 23:22:08 +0900

    upload ui

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,8 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:instagram_clone_flutter/src/components/image_data.dart';
 
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
+
+  Widget _imagePreview() {
+    return Container(
+      width: Get.width,
+      height: Get.width,
+      color: Colors.grey,
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: const [
+                Text(
+                  '갤러리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xff808080),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    const SizedBox(
+                      width: 7,
+                    ),
+                    const Text(
+                      '여러 항목 선택',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 5),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff808080),
+                ),
+                child: ImageData(IconsPath.cameraIcon),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        mainAxisSpacing: 1,
+        crossAxisSpacing: 1,
+        childAspectRatio: 1,
+      ),
+      itemCount: 50,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(color: Colors.red);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +103,35 @@ class Upload extends StatelessWidget {
             child: ImageData(IconsPath.closeImage),
           ),
         ),
-        title: Text('New Post'),
+        title: const Text(
+          'New Post',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _imagePreview(),
+            _header(),
+            _imageSelectList(),
+          ],
+        ),
       ),
     );
   }
