7ee867a278815b0f9cb2e10a3f533c9aa311dace
reve74 <reve99@naver.com>
2022-03-01 23:47:00 +0900

    upload ui

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,15 +1,96 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:instagram_clone_flutter/src/components/image_data.dart';
+import 'dart:typed_data';
 
-class Upload extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter/src/components/image_data.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class Upload extends StatefulWidget {
   const Upload({Key? key}) : super(key: key);
 
+  @override
+  State<Upload> createState() => _UploadState();
+}
+
+class _UploadState extends State<Upload> {
+  var albums = <AssetPathEntity>[]; // AssetPathEntity?
+  var imageList = <AssetEntity>[]; // AssetEntity?
+  var headerTitle = '';
+  AssetEntity? selectedImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPhoto();
+  }
+
+  void _loadPhoto() async {
+    // 사진 불러오기
+    var result = await PhotoManager.requestPermissionExtend();
+    if (result.isAuth) {
+      albums = await PhotoManager.getAssetPathList(
+        type: RequestType.image,
+        filterOption: FilterOptionGroup(
+            imageOption: const FilterOption(
+              sizeConstraint: SizeConstraint(minHeight: 100, minWidth: 100),
+            ),
+            orders: [
+              const OrderOption(type: OrderOptionType.createDate, asc: false),
+              // 최신이미지 먼저 보기
+            ]),
+      );
+      // albums.addAll([
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      //   AssetPathEntity()..name = '1',
+      // ]);
+
+      _loadData();
+    } else {
+      // message
+    }
+  }
+
+  void _loadData() async {
+    headerTitle = albums.first.name;
+    await _pagingPhotos(); // 이미지 파일이 많을경우 별도로 페이징 처리
+    update();
+  }
+
+  Future<void> _pagingPhotos() async {
+    // 사진 불러오기 페이징
+    var photos = await albums.first.getAssetListPaged(0, 30);
+    imageList.addAll(photos);
+    selectedImage = imageList.first;
+  }
+
+  void update() => setState(() {});
+
   Widget _imagePreview() {
+    // 상단의 이미지 선택
+    var width = MediaQuery.of(context).size.width;
     return Container(
-      width: Get.width,
-      height: Get.width,
+      width: width,
+      height: width,
       color: Colors.grey,
+      child: selectedImage == null
+          ? Container()
+          : _photoWidget(
+              selectedImage!,
+              width.toInt(),
+              builder: (data) {
+                return Image.memory(
+                  data,
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
     );
   }
 
@@ -19,19 +100,73 @@ class Upload extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
-              children: const [
-                Text(
-                  '갤러리',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
                 ),
-                Icon(Icons.arrow_drop_down),
-              ],
+                context: context,
+                isScrollControlled: albums.length > 10 ? true : false,
+                constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height -
+                        MediaQuery.of(context).padding.top),
+                // 상단바 높이를 제외한 높이를 차지
+                builder: (_) => Container(
+                  height: albums.length > 10
+                      ? Size.infinite.height
+                      : albums.length * 60,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 7),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.black54,
+                          ),
+                          width: 40,
+                          height: 4,
+                        ),
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: List.generate(
+                              albums.length,
+                              (index) => Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 20),
+                                child: Text(albums[index].name),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                children: [
+                  Text(
+                    headerTitle,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Icon(Icons.arrow_drop_down),
+                ],
+              ),
             ),
           ),
           Row(
@@ -82,9 +217,38 @@ class Upload extends StatelessWidget {
         crossAxisSpacing: 1,
         childAspectRatio: 1,
       ),
-      itemCount: 50,
+      itemCount: imageList.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(color: Colors.red);
+        return _photoWidget(imageList[index], 200, builder: (data) {
+          return GestureDetector(
+            onTap: () {
+              selectedImage = imageList[index];
+              update();
+            },
+            child: Opacity(
+              opacity: imageList[index] == selectedImage ? 0.3 : 1,
+              child: Image.memory(
+                data,
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        });
+      },
+    );
+  }
+
+  Widget _photoWidget(AssetEntity asset, int size,
+      {required Widget Function(Uint8List) builder}) {
+    return FutureBuilder(
+      future: asset.thumbDataWithSize(size, size),
+      builder: (_, AsyncSnapshot<Uint8List?> snapshot) {
+        if (snapshot.hasData) {
+          // 데이터가 있으면
+          return builder(snapshot.data!);
+        } else {
+          return Container();
+        }
       },
     );
   }
